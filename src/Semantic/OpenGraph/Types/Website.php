dd59a8c9ad0e19dc8b7542681d37e67be3311022
Tomáš Holan <tomas.holan@mysurface.cz>
2017-02-05 21:40:19 +0100

    Added website type

---
+++ src/Semantic/OpenGraph/Types/Website.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace holantomas\Semantic\OpenGraph\Types;
+
+use holantomas\Semantic\OpenGraph\TType;
+use holantomas\Semantic\OpenGraph\Type;
+
+
+/**
+ * @author       Tomáš Holan <mail@tomasholan.eu>, Tomáš Holan [www.tomasholan.eu]
+ * @package      holantomas/semantic
+ * @copyright    Copyright © 2016, Tomáš Holan [www.tomasholan.eu]
+ */
+class Website extends Type implements IWebsite {
+
+}
